e7e5c8ab9323f4c783d8057776421b8ffd3d3d91
tdesvenain <thomas.desvenain@gmail.com>
2012-08-07 17:24:40 +0200

    fix

--- src/ecreall/helpers/testing/base.py
+++ src/ecreall/helpers/testing/base.py
@@ -1,12 +1,13 @@
 from Products.CMFCore.utils import getToolByName
 
+from plone.app.testing import login
 
 class BaseTest(object):
     """A class with many useful helpers to test workflows
     """
 
     def login(self, user):
-        login(self.portal, user)
+        login(self.layer['portal'], user)
 
     def assertLengthEqual(self, list1, length, msg=None):
         self.assertEqual(len(list1), length, msg)
