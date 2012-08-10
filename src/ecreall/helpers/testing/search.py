ebbe7d179b0883ff39ef2a763d461eeff3fe8edb
tdesvenain <thomas.desvenain@gmail.com>
2012-08-10 23:02:42 +0200

    added a helper to get easily a brain from the catalog

--- src/ecreall/helpers/testing/search.py
+++ src/ecreall/helpers/testing/search.py
@@ -10,6 +10,10 @@ class BaseSearchTest(BaseTest):
     """A class with many useful helpers to test workflows
     """
 
+    def getBrain(self, uid):
+        ctool = self.portal.portal_catalog
+        return ctool.unrestrictedSearchResults(UID=uid)[0]
+
     def assertCanFind(self, document):
         """Check if user can find document through a catalog search
         """
@@ -23,11 +27,16 @@ class BaseSearchTest(BaseTest):
         self.assertEqual(len(ctool.searchResults(UID=document.UID())), 0)
 
     def assertSearchFinds(self, query, results_num):
+        """Check number of results of a query
+        """
         ctool = self.portal.portal_catalog
         self.assertEqual(len(ctool.unrestrictedSearchResults(**query)),
                              results_num)
 
     def assertSearchFindsInOrder(self, query, ids):
+        """Check order of the results
+        ids are the ids of documents
+        """
         ctool = self.portal.portal_catalog
         result_ids = [b.getId for b in ctool.unrestrictedSearchResults(**query)]
         self.assertListEqual(result_ids, ids)
