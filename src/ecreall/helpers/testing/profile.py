6405227653289d39656f1f052ebfdcf0c457cea3
tdesvenain <thomas.desvenain@gmail.com>
2012-08-24 19:55:42 +0200

    added profiling decorator

---
+++ src/ecreall/helpers/testing/profile.py
@@ -0,0 +1,16 @@
+import time
+from logging import getLogger
+logger = getLogger('ecreall.profile')
+
+def trace_time(func):
+    def wrapper(*__args,**__kw):
+        t = time.time()
+        try:
+            return func(*__args,**__kw)
+        finally:
+            logger.info( "End for %s in %s" % \
+                         (func.__name__,
+                          time.strftime('%H h %M m %S.%m sec',
+                                        time.gmtime(time.time() - t))))
+    return wrapper
+
