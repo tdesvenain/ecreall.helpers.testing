940daf95af520744bcd8735f6905c2a0f65ce65b
Thomas Desvenain <thomas.desvenain@gmail.com>
2013-10-07 23:17:30 +0200

    Now checks unexpected recipients of sent mail (not only missing ones).

--- src/ecreall/helpers/testing/search.py
+++ src/ecreall/helpers/testing/search.py
@@ -1,4 +1,5 @@
 from .base import BaseTest
+from Products.CMFCore.interfaces._content import IContentish
 
 
 class BaseSearchTest(BaseTest):
@@ -21,12 +22,26 @@ class BaseSearchTest(BaseTest):
         ctool = self.portal.portal_catalog
         self.assertEqual(len(ctool.searchResults(UID=document.UID())), 0)
 
-    def assertSearchFinds(self, query, results_num):
+    def assertSearchFinds(self, query, result):
         """Check number of results of a query
+           or check a document is present (by uid or object)
         """
         ctool = self.portal.portal_catalog
-        self.assertEqual(len(ctool.unrestrictedSearchResults(**query)),
-                             results_num)
+        brains = ctool.unrestrictedSearchResults(**query)
+        if type(result) == int:
+            self.assertEqual(len(brains),
+                         result, [r.getPath() for r in brains])
+        elif type(result) == str:
+            self.assertTrue(result in [r.UID for r in brains],
+                "%s document not found in results : %s" % (result,
+                                                [r.getPath() for r in brains]))
+        elif IContentish.providedBy(result):
+            self.assertTrue(IUUID(result) in [r.UID for r in brains],
+                "%s document not found in results : %s" % (
+                                            '/'.join(results.getPhysicalPath()),
+                                            [r.getPath() for r in brains]))
+        else:
+            raise ValueError("parameter must be an uid or a results num")
 
     def assertSearchFindsInOrder(self, query, ids):
         """Check order of the results
@@ -35,3 +50,6 @@ class BaseSearchTest(BaseTest):
         ctool = self.portal.portal_catalog
         result_ids = [b.getId for b in ctool.unrestrictedSearchResults(**query)]
         self.assertListEqual(result_ids, ids)
+
+    def assertListContains(self, list1, list2):
+        self.assertListEqual(sorted(list(list1)), sorted(list(list2)))
