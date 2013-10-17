f6b93c490044528106f4a652fd1c4e5d739d5785
Thomas Desvenain <thomas.desvenain@gmail.com>
2013-10-17 16:01:11 +0200

    added assertSameDay assert to compare dates   (in DateTime, string or datetime format).

--- src/ecreall/helpers/testing/search.py
+++ src/ecreall/helpers/testing/search.py
@@ -1,6 +1,10 @@
-from .base import BaseTest
-from Products.CMFCore.interfaces._content import IContentish
+import datetime
+from DateTime import DateTime
+
+from Products.CMFCore.interfaces import IContentish
 from plone.uuid.interfaces import IUUID
+
+from .base import BaseTest
 
 
 class BaseSearchTest(BaseTest):
@@ -54,3 +58,24 @@ class BaseSearchTest(BaseTest):
 
     def assertListContains(self, list1, list2):
         self.assertListEqual(sorted(list(list1)), sorted(list(list2)))
+
+    def assertSameDay(self, date1, date2):
+        date1, date2 = datify(date1), datify(date2)
+        self.assertEqual(date1.Date(), date2.Date())
+
+
+def datify(s):
+    """Get a DateTime object from a string (or anything parsable by DateTime,
+       a datetime.date, a datetime.datetime
+    """
+    if not isinstance(s, DateTime):
+        if s == 'None':
+            s = None
+        elif isinstance(s, datetime.date):
+            s = DateTime(s.year, s.month, s.day)
+        elif isinstance(s, datetime.datetime):
+            s = DateTime(s.isoformat())
+        elif s is not None:
+            s = DateTime(s)
+
+    return s
