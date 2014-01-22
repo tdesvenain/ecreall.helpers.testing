7e2af688ace3de7408bd51b16834475cfe04442f
Thomas Desvenain <thomas.desvenain@gmail.com>
2014-01-22 17:38:41 +0100

    - Added status and header methods on FakeResponse   and getURL method on FakeRequest.
    - When we get a key from Request, we try to get it from form.

--- src/ecreall/helpers/testing/base.py
+++ src/ecreall/helpers/testing/base.py
@@ -4,19 +4,62 @@ from plone.app.testing import login
 class FakeResponse(dict):
 
     redirect_url = None
+    status = 404
+    headers = None
+
+    def __init__(self):
+        self.headers = {}
+
     def redirect(self, url):
         self.redirect_url = url
 
+    def getStatus(self):
+        return self.status
+
+    def setStatus(self, status):
+        self.status = status
+
+    def getHeader(self, name):
+        return self.headers.get(name, None)
+
+    def setHeader(self, name, value):
+        self.headers[name] = value
+
 
 class FakeRequest(dict):
+    debug = False
+
+    def has_key(self, key):
+        try:
+            self.__getitem__(key)
+        except:
+            return 0
+        else:
+            return 1
+
+    def keys(self):
+        return self.form.keys()
+
+    def __contains__(self, a):
+        return a in self.form or a in self.__dict__
+
+    def get(self, k, default=None):
+        return self.form.get(k, None) or self.__dict__.get(k, None) or default
+
+    def __getitem__(self, key):
+        return self.get(key)
 
     def __init__(self):
         self.form = {}
         self.response = FakeResponse()
+        self.URL = ''
 
     @property
     def RESPONSE(self):
         return self.response
+
+    def getURL(self):
+        return self.URL
 
 
 class BaseTest(object):
